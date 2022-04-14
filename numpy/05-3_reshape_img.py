90201df2259821dcad08ed614c6ea30f853a667a
yxing11 <yxing11@usfca.edu>
2022-04-14 12:05:28 -0700

    update

---
+++ numpy/05-3_reshape_img.py
@@ -0,0 +1,24 @@
+# suppose we have a RGB image with length * width * depth (=3).
+# Write a function to unroll it to a 1D vector.
+# What if we have a list of images?
+import numpy as np
+
+t_image = np.array([[[ 0.67826139,  0.29380381],
+                     [ 0.90714982,  0.52835647],
+                     [ 0.4215251 ,  0.45017551]],
+
+                   [[ 0.92814219,  0.96677647],
+                    [ 0.85304703,  0.52351845],
+                    [ 0.19981397,  0.27417313]],
+
+                   [[ 0.60659855,  0.00533165],
+                    [ 0.10820313,  0.49978937],
+                    [ 0.34144279,  0.94630077]]])
+
+t_images = np.array([t_image, t_image, t_image])
+
+def image2vector(image):
+    v = image.reshape(image.shape[0], -1)
+    return v
+
+print(image2vector(t_images))
