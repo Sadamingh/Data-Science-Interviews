e310083ba5585f000051432be326b2ea15b60e76
yxing11 <yxing11@usfca.edu>
2022-04-29 11:17:35 -0700

    update

--- algos/sort/04 insertionsort_simple.py
+++ algos/sort/04 insertionsort_simple.py
@@ -1,15 +1,16 @@
 def insertion_sort(a):
 
     for i in range(1, len(a)):
-
-        val = a[i]
-
-        while i >= 1 and val < a[i-1]:
-            a[i] = a[i-1]
-            i -= 1
-
-        a[i] = val
-
+        move_forward(i, a)
     return a
+
+
+def move_forward(i, a):
+    val = a[i]
+    while i >= 1 and val < a[i-1]:
+        a[i] = a[i-1]
+        i -= 1
+    a[i] = val
+
 
 print(insertion_sort([5,1,2,8,4,7]))
