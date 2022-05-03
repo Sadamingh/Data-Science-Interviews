b2ccf4eb387f7125def55dfc151d0921d55fbfe9
yxing11 <yxing11@usfca.edu>
2022-05-03 10:41:57 -0700

    update

--- algos/sort/08 mergesort.py
+++ algos/sort/08 mergesort.py
@@ -2,13 +2,13 @@ from mergeTwoSortedArray import mergeTwoSortedArray
 
 def merge_sort(data):
 
-    if len(data)==0:
+    if len(data) == 0:
         return []
 
-    elif len(data)==1:
+    elif len(data) == 1:
         return [data[0]]
 
-    elif len(data)==2:
+    elif len(data) == 2:
         if data[0]<data[1]:
             return data
         return [data[1], data[0]]
@@ -21,5 +21,5 @@ def merge_sort(data):
 
 print(merge_sort([]))
 print(merge_sort([1]))
-print(merge_sort([2,1]))
+print(merge_sort([2, 1]))
 print(merge_sort([5,1,2,8,4,7]))
