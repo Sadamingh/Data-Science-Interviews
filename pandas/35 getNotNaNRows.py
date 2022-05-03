27579a245de790514155c5e59c7f2f62bb14eccb
yxing11 <yxing11@usfca.edu>
2022-05-03 09:48:14 -0700

    update

---
+++ pandas/35 getNotNaNRows.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                  columns=['a', 'b', 'c'])
+
+df = df.mask(df > 5)
+ans = df[~df.a.isnull()]
+
+print(ans)
