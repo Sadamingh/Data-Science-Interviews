9346d288094961da2353b19cfd76dc447843c1f3
yxing11 <yxing11@usfca.edu>
2022-05-02 23:32:29 -0700

    update

---
+++ pandas/10 getCol.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[1, 2, 3], [1, 2, 3], [7, 8, 9]]),
+                  columns=['a', 'b', 'c'])
+
+ans = list(df['a'])
+
+print(ans)
