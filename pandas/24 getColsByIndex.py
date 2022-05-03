9572744300908dde65b6a77556e9d7d3ef7ccd23
yxing11 <yxing11@usfca.edu>
2022-05-03 09:26:58 -0700

    update

---
+++ pandas/24 getColsByIndex.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[1, 2, 3, 'a'], [4, 5, 6, 'b'], [7, 8, 9, 'c']]),
+                  columns=['a', 'b', 'c', 'index'])
+
+df = df.set_index('index')
+
+ans = df[['a', 'b']]
+
+print(ans)
