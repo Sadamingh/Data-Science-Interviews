b5c4a008ebc7a80ca575d31f3e8f8cbf89eaab88
yxing11 <yxing11@usfca.edu>
2022-05-03 09:53:38 -0700

    update

---
+++ pandas/36 sumWithNaNsNoSkip.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[1, np.NaN, 3], [4, 5, 6], [7, 8, np.NaN]]),
+                  columns=['a', 'b', 'c'])
+
+ans = df.sum(axis=1, skipna=False)
+
+print(ans)
---
+++ pandas/39 getColsContainsNaN.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[1, np.NaN, 3], [4, 5, 6], [7, 8, np.NaN]]),
+                  columns=['a', 'b', 'c'])
+
+ans = df.loc[:, df.isnull().any(axis=0)]
+
+print(ans)
