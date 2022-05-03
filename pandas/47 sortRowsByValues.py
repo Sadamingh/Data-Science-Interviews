675e43557f9f87424e40e9545b6257305dbae029
yxing11 <yxing11@usfca.edu>
2022-05-03 10:18:20 -0700

    update

---
+++ pandas/47 sortRowsByValues.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.array([[9, 2, 3, 'a'], [4, 5, 6, 'c'], [7, 8, 9, 'b']]),
+                  columns=['a', 'b', 'c', 'index'])
+
+df = df.set_index('index')
+
+ans = df.sort_values(by=['a'])
+
+print(ans)
