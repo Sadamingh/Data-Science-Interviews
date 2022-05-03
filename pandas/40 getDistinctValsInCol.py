b36fa7cbfee1304ca1613e3dd4d112b7af58729a
yxing11 <yxing11@usfca.edu>
2022-05-03 09:59:01 -0700

    update

--- pandas/40 getDistinctValsInCol.py
+++ pandas/40 getDistinctValsInCol.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.array([[1, np.NaN, 3], [4, 5, 6], [7, 8, np.NaN]]),
+df = pd.DataFrame(np.array([[1, np.NaN, 3], [1, 5, 6], [7, 8, np.NaN]]),
                   columns=['a', 'b', 'c'])
 
 ans = df.a.unique()
