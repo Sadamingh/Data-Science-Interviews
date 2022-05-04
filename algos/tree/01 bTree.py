b3c68261ea5b010c8eaf0fdd944bb625098833a0
yxing11 <yxing11@usfca.edu>
2022-05-03 20:29:11 -0700

    update

---
+++ algos/tree/01 bTree.py
@@ -0,0 +1,6 @@
+import binarytree
+
+tree = [5, 1, 4, None, None, 3, 6]
+BST = binarytree.build(tree)
+
+print(BST)
---
+++ algos/tree/02 BST.py
@@ -0,0 +1,6 @@
+import binarytree
+
+tree = [5, 2, 7, 4, 6, None, 14]
+BST = binarytree.build(tree)
+
+print(BST)
