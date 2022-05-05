0a5b6c7d70a6911e1bbe492c9ac6e0c671f00c2b
yxing11 <yxing11@usfca.edu>
2022-05-04 20:47:25 -0700

    update

---
+++ algos/karat/solitaire.py
@@ -0,0 +1,13 @@
+# find the the number of connected same value
+# grid1 = [[4, 4, 4, 4],
+#          [5, 5, 5, 4],
+#          [2, 5, 7, 5]]
+# Case 1 --------------------
+# x, y = 0, 0
+# output = 5
+# Case 2 --------------------
+# x, y = 1, 0
+# output = 4
+# Case 3 --------------------
+# x, y = 1, 1
+# output = 4
