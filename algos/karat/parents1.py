468576945e0fdb68ca8b16c780d37758be1856ad
yxing11 <yxing11@usfca.edu>
2022-05-05 00:40:53 -0700

    update

--- algos/karat/parents1.py
+++ algos/karat/parents1.py
@@ -5,3 +5,21 @@
 #                 \
 #                  7
 # output a list of nodes with 0 parents or 1 parents
+
+from collections import defaultdict
+
+def func(a):
+    ans = []
+    hashmap = defaultdict(set)
+
+    for x in a:
+        if x[0] not in hashmap: hashmap[x[0]]
+        hashmap[x[1]].add(x[0])
+
+    for x in hashmap:
+        if len(hashmap[x]) <= 1:
+            ans.append(x)
+
+    return ans
+
+print(func([[1,4], [1,5], [2,5], [3,6], [6,7]]))
