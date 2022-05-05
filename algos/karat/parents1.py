2f454aad1feff6fe88b22404e131ed607dd853ea
yxing11 <yxing11@usfca.edu>
2022-05-04 18:55:38 -0700

    update

---
+++ algos/karat/parents1.py
@@ -0,0 +1,7 @@
+# Given a plot {{1,4}, {1,5}, {2,5}, {3,6}, {6,7}}
+#    1    2    3
+#  /  \  /      \
+# 4    5         6
+#                 \
+#                  7
+# output a list of nodes with 0 parents or 1 parents
---
+++ algos/karat/parents2.py
@@ -0,0 +1,13 @@
+# Given a plot {{1,4}, {1,5}, {2,5}, {3,6}, {6,7}}
+#    1    2    3
+#  /  \  /      \
+# 4    5         6
+#                 \
+#                  7
+# Check if two nodes shares the same ancestor
+# Case 1 --------------------
+# input = 4, 5
+# output = True
+# Case 2 --------------------
+# input = 2, 6
+# output = False
