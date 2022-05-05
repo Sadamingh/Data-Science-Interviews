0fcefa47ee6a49818b26365aba8247c958a6ad8f
yxing11 <yxing11@usfca.edu>
2022-05-04 21:53:46 -0700

    update

---
+++ algos/karat/course3.py
@@ -0,0 +1,3 @@
+# Quest2 follow-up:
+# Assuming that each course can have multiple prerequisites,
+# find out the names of all the courses halfway through the path
---
+++ algos/karat/treasure2.py
@@ -0,0 +1,6 @@
+# -1 is wall and 0 is path, can we go to all the 0s?
+# map = [[-1,0,-1,-1],
+#        [0,0,0,0],
+#        [-1,-1,0,-1],
+#        [0,-1,0,0]]
+# output = False
