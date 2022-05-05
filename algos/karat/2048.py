79e5cbf354313736f7d3fd095705bdd6c528c7bf
yxing11 <yxing11@usfca.edu>
2022-05-05 00:30:44 -0700

    update

--- algos/karat/2048.py
+++
@@ -1,7 +0,0 @@
-# Implement a game like 2048, left swipe
-# Case 1 --------------------
-# input = [2,2,2,2]
-# output = [8,0,0,0]
-# Case 2 --------------------
-# input = [0,2,2,0]
-# output = [4,0,0,0]
