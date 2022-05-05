d0de9c8dcf3949e7829ffa616feeb20d5c2310b4
yxing11 <yxing11@usfca.edu>
2022-05-05 01:08:44 -0700

    update

--- algos/karat/parents3.py
+++ algos/karat/parents3.py
@@ -11,3 +11,22 @@
 # Case 2 --------------------
 # input = 4
 # output = 1
+
+def func(a, target):
+    maxVal = max(max(a))
+    map = list(range(maxVal+1))
+
+    for p, c in a:
+        map[c] = p
+
+    x = map[target]
+
+    while True:
+        if target == x:
+            return target
+        else:
+            target = map[x]
+            x = map[target]
+
+
+print(func([[1,4], [1,5], [2,5], [3,6], [6,7], [0,1]], 5))
