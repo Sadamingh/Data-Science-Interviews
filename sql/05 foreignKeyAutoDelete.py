73cbdc2763d9cb48ad3fa9e6b8e0a6e17b62902f
yxing11 <yxing11@usfca.edu>
2022-05-07 19:23:37 -0700

    update

---
+++ sql/05 foreignKeyAutoDelete.py
@@ -0,0 +1,30 @@
+# Create a faculty table in the current database with fid (int),
+# fname (string), department (string)
+# primary key: fid
+# foreign key: department refer to the column in student table
+# change the restrict rules to auto delete
+
+query = """
+CREATE TABLE IF NOT EXISTS faculty (
+    fid NUMBER(5),
+    fname VARCHAR(20),
+    department VARCHAR(20),
+    PRIMARY KEY (fid),
+    FOREIGN KEY (department) REFERENCES student (department)
+    ON DELETE CASCADE
+);
+"""
+
+# Do NOT change ------------------------
+check = """
+SELECT name
+FROM sqlite_master;
+"""
+
+from utils import *
+
+cur, con = get_school_db_2()
+cur.execute(query)
+cur.execute(check)
+print(cur.fetchall())
+con.close()
