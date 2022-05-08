43ed6d8b8148cf7bc1d1d3388bb0e74a3a78989a
yxing11 <yxing11@usfca.edu>
2022-05-08 12:45:29 -0700

    update

---
+++ sklearn/preprocessing/13 binning.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.preprocessing import KBinsDiscretizer
+
+X = np.array([[ -3., 5., 15 ],
+              [  0., 6., 14 ],
+              [  6., 3., 11 ]])
+
+bin = KBinsDiscretizer(n_bins=[3, 2, 2], encode='ordinal')
+bin.fit(X)
+X = bin.transform(X)
+
+print(X)
