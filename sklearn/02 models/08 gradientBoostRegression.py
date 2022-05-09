30811cc7c5a02301834e4859cd3141b1f3c1bf8b
yxing11 <yxing11@usfca.edu>
2022-05-08 20:50:09 -0700

    update

--- sklearn/02 models/08 gradientBoostRegression.py
+++ sklearn/02 models/08 gradientBoostRegression.py
@@ -3,7 +3,7 @@ X, y = load_diabetes(return_X_y=True)
 
 from sklearn.ensemble import GradientBoostingRegressor
 
-gbr = GradientBoostingRegressor()
+gbr = GradientBoostingRegressor(n_estimators=100)
 gbr.fit(X, y)
 
 print(gbr.predict(X))
