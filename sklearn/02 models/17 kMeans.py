43cfec945f310cd93650341373746ce3c12e61d6
yxing11 <yxing11@usfca.edu>
2022-05-08 21:55:37 -0700

    update

---
+++ sklearn/02 models/17 kMeans.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import load_iris
+X, y = load_iris(return_X_y=True)
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+X = StandardScaler().fit_transform(X)
+
+km = KMeans(n_clusters=3, init='random')
+km.fit(X)
+
+print(km.predict(X))
