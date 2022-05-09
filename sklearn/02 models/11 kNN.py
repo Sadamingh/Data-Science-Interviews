7272e14249e491ed208dd8a112c631a4381a531a
yxing11 <yxing11@usfca.edu>
2022-05-08 21:41:12 -0700

    update

--- sklearn/02 models/11 kNN.py
+++ sklearn/02 models/11 kNN.py
@@ -1,12 +1,11 @@
 from sklearn.datasets import load_iris
 X, y = load_iris(return_X_y=True)
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 
 X = StandardScaler().fit_transform(X)
+kNN = KNeighborsClassifier(n_neighbors=3)
+kNN.fit(X, y)
 
-logit = LogisticRegression()
-logit.fit(X, y)
-
-print(logit.predict(X))
+print(kNN.predict(X))
