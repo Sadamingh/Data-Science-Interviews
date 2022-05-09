d1c598812f8bb21a019cf0b70d7fb0bf7543e4b4
yxing11 <yxing11@usfca.edu>
2022-05-08 17:09:24 -0700

    update

---
+++ sklearn/preprocessing/19 robustCovAnomalyDetection.py
@@ -0,0 +1,16 @@
+import numpy as np
+true_cov = np.array([[.5, .6],[.6, .4]])
+X = np.random.RandomState(0).multivariate_normal(mean=[0, 0], cov=true_cov,size=500)
+
+from sklearn.covariance import EllipticEnvelope
+cov = EllipticEnvelope().fit(X)
+isOutlier = cov.predict(X)
+
+import matplotlib.pyplot as plt
+for (x, y), c in zip(X, isOutlier):
+    if c == -1:
+        plt.scatter(x, y, color="r")
+    else:
+        plt.scatter(x, y, color="b")
+plt.title("red = outlier, blue = inlier")
+plt.show()
