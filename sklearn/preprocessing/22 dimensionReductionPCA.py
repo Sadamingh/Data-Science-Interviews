09b3f7f55cbe7d178eacd67789d6e8aee3b20cad
yxing11 <yxing11@usfca.edu>
2022-05-08 17:30:03 -0700

    update

--- sklearn/preprocessing/22 dimensionReductionPCA.py
+++ sklearn/preprocessing/22 dimensionReductionPCA.py
@@ -3,7 +3,11 @@ X = np.random.rand(20, 1000)
 
 print(X.shape)
 
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+
+scaler = StandardScaler().fit(X)
+X = scaler.transform(X)
 
 pca = PCA(n_components=5)
 pca.fit(X)
