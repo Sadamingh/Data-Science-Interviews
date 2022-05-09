f5bde43cf2055573002032b142cb47c4f32f8bc5
Yufeng(Adam) Xing <42868592+Sadamingh@users.noreply.github.com>
2022-05-09 00:05:18 -0700

    Update 02 trainTestSplit.py

--- sklearn/01 preprocessing/02 trainTestSplit.py
+++ sklearn/01 preprocessing/02 trainTestSplit.py
@@ -10,6 +10,6 @@ X = np.array([[ 1., -1.,  2.],
 y = np.array([1, 2, 3, 4, 5])
 
 
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 
-print(X_train, y_train, X_test, y_test)
+print(X_train, X_test, y_train, y_test)
